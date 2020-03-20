4071c82303f05de55fa7f2fa9f85d5a40d8a0980
Christian Knauer <christiancalled@gmail.com>
2020-03-20 14:56:16 -0700

    Set fixed header via JS.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 // START HIDE NAV ITEMS OF REGULAR NAV.
 // SHOW BURGER-NAV.
 {
-
   const mainNav = document.getElementsByClassName('nav-main')[0];
   const liArr = mainNav.getElementsByTagName('li');
   const burger = mainNav.getElementsByClassName('nav-burger')[0];
@@ -21,8 +20,7 @@
   }
 
 
-  window.onresize = function () {
-
+  window.addEventListener('resize', function () {
     for (let i = 0; i < liArr.length; i++) {
       // If li not the logo.
       if (!liArr[i].classList.contains('logo')) {
@@ -36,9 +34,38 @@
         }
       }
     }
-  };
+  });
+
 
   // Show burger-nav.
   burger.classList.remove('hide');
 
 } // End hide nav items of regular nav. Show burger-nav.
+
+
+
+
+// START FIXED HEADER.
+// INSERT DIV AFTER HEADER W/SAME HEIGHT AS HEADER.
+{
+
+  const header = document.getElementsByTagName('header')[0];
+  const headerHeight = window.getComputedStyle(header).height;
+  const fixedHeaderPushDown = document.createElement('div');
+
+  header.style.position = 'fixed';
+
+
+  // Set push-down-divs height to header height.
+  fixedHeaderPushDown.style.height = headerHeight;
+
+  // Insert push-down-div after header.
+  document.getElementsByTagName('body')[0].insertBefore(fixedHeaderPushDown, header.nextSibling);
+
+
+  window.addEventListener('resize', function () {
+    // Set push-down-divs height to header height.
+    fixedHeaderPushDown.style.height = window.getComputedStyle(header).height;
+  });
+
+} // End fixed header.
