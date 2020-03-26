a58ad6c2ce7f8988b9e6b7af447119c37aff3a97
Christian Knauer <christiancalled@gmail.com>
2020-03-26 12:21:37 -0700

    Implement - logic for header - logic for overlay menu

--- js/script.js
+++ js/script.js
@@ -38,115 +38,95 @@
 } // End hide nav items of regular nav. Show burger-nav.
 
 
-
-
-// START FIXED HEADER.
-// INSERT DIV AFTER HEADER W/SAME HEIGHT AS HEADER.
+// START HEADER AND OVERLAY MENU.
 {
-
   const header = document.getElementsByTagName('header')[0];
-  const headerHeight = window.getComputedStyle(header).height;
-  const fixedHeaderPushDown = document.createElement('div');
-  const navBurger = document.getElementsByClassName('nav-burger')[0];
-
-  fixedHeaderPushDown.classList.add('header-push-down');
-
-  header.style.position = 'fixed';
-  navBurger.classList.add('js-nav-burger');
-
-  // Set push-down-divs height to header height.
-  fixedHeaderPushDown.style.height = headerHeight;
-
-  // Insert push-down-div after header.
-  document.getElementsByTagName('body')[0].insertBefore(fixedHeaderPushDown, header.nextSibling);
-
-
-  window.addEventListener('resize', function () {
-    // Set push-down-divs height to header height.
-    fixedHeaderPushDown.style.height = window.getComputedStyle(header).height;
-  });
-
-} // End fixed header.
-
-
-
-
-// DISPLAY HEADER
-// - STATIC WHEN SCROLLING DOWN,
-// - FIXED WHEN SCROLLING UP.
-{
-  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
-  const header = document.getElementsByTagName('header')[0];
+  const headerBuffer = document.getElementsByClassName('headerBuffer')[0];
   let headerHeight = header.offsetHeight;
-  let fixedHeaderPushDown = document.getElementsByClassName('header-push-down')[0];
+console.log(headerHeight);
+
+  // WHEN SCROLLED PAST CERTAIN POINT TURN STATIC HEADER INTO FIXED HEADER
+  let headerPositioning_1 = () => {
+    if (window.pageYOffset > 500) {
+      header.style.position = 'fixed';
+      headerBuffer.style.height = headerHeight + 'px';
+
+    } else if (window.pageYOffset === 0) {
+      header.style.position = 'static';
+      headerBuffer.style.height = 0;
+      header.classList.remove('js-header-transition');
+    }
+  };
+  window.addEventListener('scroll', headerPositioning_1);
 
 
-  // Update header height according to screen width.
-  window.addEventListener('resize', function () {
-    headerHeight = header.offsetHeight;
-  });
+
+  // ON LOAD, SET HEADER TO FIXED/STATIC DEPENDING ON SCROLL POSITION
+  let headerPositioning_2 = () => {
+
+    if (window.pageYOffset > 500) {
+      header.style.position = 'fixed';
+      headerBuffer.style.height = headerHeight + 'px';
+      header.classList.add('js-header-transition');
+    } else {
+      header.style.position = 'static';
+      headerBuffer.style.height = 0;
+    }
+  };
+  window.addEventListener('load', headerPositioning_2);
 
 
-  function staticFixedHeader() {
+
+  // SHOW/HIDE FIXED HEADER DEPENDING ON SCROLL DIRECTION
+  let lastScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  let showHideHeader = () => {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    // If downscroll.
-    if (scrollTop > lastScrollTop) {
-      // If scroll from top less than header height.
-      if (scrollTop < (8 * headerHeight + 2)) {
-        header.style.position = 'static';
-        fixedHeaderPushDown.style.display = 'none';
-      } else {
-        // Move header up.
-        header.style.transform = `translateY(-${headerHeight}px)`;
-      }
-
-    // If upscroll.
-    } else if (scrollTop < lastScrollTop) {
-      // If scroll from top larger than header height.
-      if (scrollTop > (8 * headerHeight)) {
-        header.style.position = 'fixed';
-        fixedHeaderPushDown.style.display = 'block';
-        // Move header down
-        header.style.transform = 'translateY(0px)';
-      }
+    if (header.style.position === 'static' && scrollTop > headerHeight) {
+      header.style.transform = 'translateY(-' + headerHeight + 'px)';
+    } else if ((header.style.position === 'static' && scrollTop < headerHeight)) {
+      header.style.transform = 'translateY(0)';
     }
-    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-  } 
 
-  window.addEventListener('scroll', staticFixedHeader);
-
-} // End display header static/fixed.
-
-
-
-// START SHOW/HIDE MOBILE-NAV.
-{
-  let mobNav = document.getElementsByClassName('nav-mobile')[0];
-  let header = document.getElementsByTagName('header')[0];
-  let fixedHeaderPushDown = document.getElementsByClassName('header-push-down')[0];
-  let navBurger = document.getElementsByClassName('nav-burger')[0];
-  let logo = document.getElementsByClassName('logo')[0];
+    if (scrollTop > lastScrollTop && header.style.position == 'fixed' && scrollTop > headerHeight) {
+      header.style.transform = 'translateY(-' + headerHeight + 'px)';
+      header.classList.add('js-header-transition');
+    } else if (scrollTop < lastScrollTop && header.style.position !== 'static') {
+      header.style.transform = 'translateY(0)';
+      header.classList.add('js-header-transition');
+    }
+    lastScrollTop = scrollTop <= 0 ? 0 : scrollTop;
+  }
+  window.addEventListener('scroll', showHideHeader);
 
 
-  document.getElementsByClassName('nav-burger')[0].addEventListener('click', function () {
-    
-    if (!mobNav.classList.contains('nav-mobile-show')) {
-      mobNav.classList.add('nav-mobile-show');
 
+  // OVERLAY-MENU
+  const burger = document.getElementsByClassName('nav-burger')[0];
+  const overlay = document.getElementsByClassName('nav-mobile')[0];
 
-      window.removeEventListener('scroll', staticFixedHeader);
-
-      navBurger.classList.add('js-navBurger-darkBlue');
-
+  let menuOverlay = () => {
+    if (!overlay.classList.contains('js-nav-mobile-show')) {
+      overlay.classList.add('js-nav-mobile-show');
+      header.style.position = 'fixed';
+      headerBuffer.style.height = headerHeight + 'px';
+      header.style.transform = 'translateY(0)';
+      window.removeEventListener('scroll', headerPositioning_1);
+      window.removeEventListener('scroll', showHideHeader);
     } else {
-      mobNav.classList.remove('nav-mobile-show');
+      overlay.classList.remove('js-nav-mobile-show');
+      window.addEventListener('scroll', headerPositioning_1);
+      window.addEventListener('scroll', showHideHeader);
 
-      window.addEventListener('scroll', staticFixedHeader);
-
-      navBurger.classList.remove('js-navBurger-darkBlue');
-
+      // if (window.pageYOffset === 0) {
+      //   header.style.position = 'static';
+      //   headerBuffer.style.height = 0;
+      //   header.classList.remove('js-header-transition');
+      // } else {
+      //   header.classList.add('js-header-transition');
+      // }
     }
-  });
-
-} // End show/hide mobile-nav.
+  };
+  burger.addEventListener('click', menuOverlay);
+} // End header and overlay menu.
